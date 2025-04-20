fbad2e59f54ee06655a3f93c6f98c1f8c38fb11a
Xolmo <a20220378@pucp.edu.pe>
2025-04-20 12:30:29 -0500

    Proyecto IntelliJ

---
+++ GTICS_TA/src/main/java/com/example/gtics_ta/GticsTaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gtics_ta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsTaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GticsTaApplication.class, args);
+	}
+
+}
---
+++ GTICS_TA/src/test/java/com/example/gtics_ta/GticsTaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_ta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsTaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
