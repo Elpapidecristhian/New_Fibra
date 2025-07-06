736b91959a5a64851359bfaf4383e274f1b47f82
Xolmo <a20220378@pucp.edu.pe>
2025-07-06 18:57:40 -0500

    Mail

--- GTICS_TA/src/main/java/com/example/gtics_ta/Services/MailService.java
+++ GTICS_TA/src/main/java/com/example/gtics_ta/Services/MailService.java
@@ -1,8 +1,10 @@
 package com.example.gtics_ta.Services;
 
 import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
 @Service
@@ -19,6 +22,9 @@ public class MailService {
     private JavaMailSender mailSender;
     @Autowired
     private TemplateEngine templateEngine;
+    @Value("${spring.mail.from}")
+    private String remitente;
+
 
     public void enviarCorreoHtml(String para, String asunto, String htmlContent){
         try {
@@ -46,6 +52,7 @@ public class MailService {
     public void enviarCorreoConPlantilla(String para, String asunto, String plantilla, Map<String, Object> variables) {
         try {
             MimeMessage mensaje = mailSender.createMimeMessage();
+            mensaje.setFrom(new InternetAddress(remitente, "San Miguel Deportes"));
             MimeMessageHelper helper = new MimeMessageHelper(mensaje, true, "UTF-8");
 
             Context context = new Context();
@@ -59,6 +66,8 @@ public class MailService {
             mailSender.send(mensaje);
         } catch (MessagingException e) {
             throw new RuntimeException("Error al enviar correo con plantilla", e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
         }
     }
 }
